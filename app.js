9667b536afd33ea7ab595fee42ba9a049625f9d7
sunnyWR <sunny@gleecus.com>
2021-08-16 12:11:59 +0530

    pushing code for demo app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res) => {
+    res.send('<h1>Auto scaling from Demo App</h1><h4>Message:Success</h4><p>Version: 1.0.0</p>');
+});
+
+app.listen(port, () => {
+    console.log(`Demo App is up and listening to port ${port}`);
+});
+
